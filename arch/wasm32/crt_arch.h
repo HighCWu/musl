32b163fb0cb7c54500f0f79274bb0f7685d92b78
Thomas Stokes <git@tombl.dev>
2026-07-27 23:29:23 +0800

    wasm: retrieve process arguments through a syscall
    Replace the private kernel module imports with one arch-specific syscall. Pass the fixed 256 KiB buffer capacity to the kernel so an ABI limit mismatch fails safely instead of writing beyond libc's startup storage.
    
    Kernel ABI: https://github.com/tombl/linux/pull/39
    Regression coverage: https://github.com/tombl/distro/pull/117

--- arch/wasm32/crt_arch.h
+++ arch/wasm32/crt_arch.h
@@ -1,11 +1,7 @@
 #include "process_args.h"
+#include "syscall.h"
 
-__attribute__((import_module("linux"), import_name("get_args_length")))
-int wasm_get_args_length(void);
-__attribute__((import_module("linux"), import_name("get_args")))
-int wasm_get_args(struct wasm_process_args *buf);
-
-// TODO: malloc this once mmap is fixed
+// The wasm process ABI caps the complete argument blob at four 64 KiB pages.
 static char args_buf[65536 * 4];
 hidden struct wasm_process_args *__wasm_process_args = (void*)args_buf;
 
@@ -21,11 +17,8 @@ void __wasm_call_ctors(void);
 int __main_void(void);
 hidden void _start_c(long *p);
 hidden void _start(void) {
-	int len = wasm_get_args_length();
-	if (len < 0) __builtin_trap();
-	if (len > sizeof(args_buf)) __builtin_trap();
-
-	if (wasm_get_args(__wasm_process_args) < 0) __builtin_trap();
+	if (__syscall(SYS_wasm_get_args, __wasm_process_args,
+	    sizeof(args_buf)) < 0) __builtin_trap();
 
 	__init_libc(__wasm_process_args->envp, __wasm_process_args->argv[0]);
 	__libc_start_init();
