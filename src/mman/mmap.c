b368b62a769c79cfdd2128df6763981ae3815b7d
HighCWu <HighCWu@163.com>
2026-09-26 08:31:02 +0800

    wasm: support partial anonymous munmap

--- src/mman/mmap.c
+++ src/mman/mmap.c
@@ -18,8 +18,14 @@ weak_alias(dummy, __vm_wait);
 
 #ifdef __wasm__
 
+struct wasm_allocation {
+	struct wasm_allocation *next_free;
+	size_t references;
+};
+
 struct wasm_mapping {
 	struct wasm_mapping *next;
+	struct wasm_allocation *allocation;
 	void *address;
 	size_t length;
 };
@@ -31,7 +37,8 @@ void *__mmap(void *start, size_t len, int prot, int flags, int fd, off_t off)
 {
 	const int supported_flags = MAP_PRIVATE | MAP_ANONYMOUS;
 	struct wasm_mapping *mapping;
-	uintptr_t allocation, address;
+	struct wasm_allocation *allocation;
+	uintptr_t allocation_start, address;
 	size_t rounded, total;
 	int lock_error;
 
@@ -54,17 +61,25 @@ void *__mmap(void *start, size_t len, int prot, int flags, int fd, off_t off)
 		return MAP_FAILED;
 	}
 	rounded = (len + PAGESIZE - 1) & -(size_t)PAGESIZE;
-	if (rounded > SIZE_MAX - PAGESIZE - sizeof(*mapping)) {
+	if (rounded > SIZE_MAX - PAGESIZE - sizeof(*allocation)) {
 		errno = ENOMEM;
 		return MAP_FAILED;
 	}
-	total = rounded + PAGESIZE + sizeof(*mapping);
-	mapping = __libc_malloc(total);
-	if (!mapping)
+	total = rounded + PAGESIZE + sizeof(*allocation);
+	allocation = __libc_malloc(total);
+	if (!allocation)
 		return MAP_FAILED;
+	mapping = __libc_malloc(sizeof(*mapping));
+	if (!mapping) {
+		__libc_free(allocation);
+		return MAP_FAILED;
+	}
 
-	allocation = (uintptr_t)(mapping + 1);
-	address = (allocation + PAGESIZE - 1) & -(uintptr_t)PAGESIZE;
+	allocation_start = (uintptr_t)(allocation + 1);
+	address = (allocation_start + PAGESIZE - 1) & -(uintptr_t)PAGESIZE;
+	allocation->next_free = 0;
+	allocation->references = 1;
+	mapping->allocation = allocation;
 	mapping->address = (void *)address;
 	mapping->length = rounded;
 	memset(mapping->address, 0, rounded);
@@ -72,6 +87,7 @@ void *__mmap(void *start, size_t len, int prot, int flags, int fd, off_t off)
 	lock_error = pthread_mutex_lock(&mappings_lock);
 	if (lock_error) {
 		__libc_free(mapping);
+		__libc_free(allocation);
 		errno = lock_error;
 		return MAP_FAILED;
 	}
@@ -85,7 +101,10 @@ void *__mmap(void *start, size_t len, int prot, int flags, int fd, off_t off)
 
 hidden int __wasm_munmap(void *start, size_t len)
 {
-	struct wasm_mapping **cursor, *mapping = 0;
+	struct wasm_mapping **cursor, *mapping, *removed = 0;
+	struct wasm_mapping *spare;
+	struct wasm_allocation *allocation, *released = 0;
+	uintptr_t begin = (uintptr_t)start, end;
 	size_t rounded;
 	int lock_error;
 
@@ -95,26 +114,77 @@ hidden int __wasm_munmap(void *start, size_t len)
 		return -1;
 	}
 	rounded = (len + PAGESIZE - 1) & -(size_t)PAGESIZE;
-
-	lock_error = pthread_mutex_lock(&mappings_lock);
-	if (lock_error) {
-		errno = lock_error;
-		return -1;
-	}
-	for (cursor = &mappings; *cursor; cursor = &(*cursor)->next) {
-		if ((*cursor)->address == start && (*cursor)->length == rounded) {
-			mapping = *cursor;
-			*cursor = mapping->next;
-			break;
-		}
-	}
-	(void)pthread_mutex_unlock(&mappings_lock);
-
-	if (!mapping) {
+	if (begin > UINTPTR_MAX - rounded) {
 		errno = EINVAL;
 		return -1;
 	}
-	__libc_free(mapping);
+	end = begin + rounded;
+	spare = __libc_malloc(sizeof(*spare));
+
+	lock_error = pthread_mutex_lock(&mappings_lock);
+	if (lock_error) {
+		__libc_free(spare);
+		errno = lock_error;
+		return -1;
+	}
+	for (cursor = &mappings; (mapping = *cursor); ) {
+		uintptr_t mapping_begin = (uintptr_t)mapping->address;
+		uintptr_t mapping_end = mapping_begin + mapping->length;
+
+		if (end <= mapping_begin || begin >= mapping_end) {
+			cursor = &mapping->next;
+			continue;
+		}
+		if (begin <= mapping_begin && end >= mapping_end) {
+			*cursor = mapping->next;
+			mapping->next = removed;
+			removed = mapping;
+			allocation = mapping->allocation;
+			if (!--allocation->references) {
+				allocation->next_free = released;
+				released = allocation;
+			}
+			continue;
+		}
+		if (begin <= mapping_begin) {
+			mapping->address = (void *)end;
+			mapping->length = mapping_end - end;
+			cursor = &mapping->next;
+			continue;
+		}
+		if (end >= mapping_end) {
+			mapping->length = begin - mapping_begin;
+			cursor = &mapping->next;
+			continue;
+		}
+		if (!spare) {
+			(void)pthread_mutex_unlock(&mappings_lock);
+			errno = ENOMEM;
+			return -1;
+		}
+		spare->next = mapping->next;
+		spare->allocation = mapping->allocation;
+		spare->address = (void *)end;
+		spare->length = mapping_end - end;
+		mapping->next = spare;
+		mapping->length = begin - mapping_begin;
+		mapping->allocation->references++;
+		spare = 0;
+		break;
+	}
+	(void)pthread_mutex_unlock(&mappings_lock);
+
+	__libc_free(spare);
+	while (removed) {
+		mapping = removed;
+		removed = mapping->next;
+		__libc_free(mapping);
+	}
+	while (released) {
+		allocation = released;
+		released = allocation->next_free;
+		__libc_free(allocation);
+	}
 	return 0;
 }
 
